6eca9e3d3e778bb1329b60736061bde9685e5df8
Raphael-Maximowski <raphaelmxjv@gmail.com>
2025-01-22 19:26:42 -0300

    feat: Header

---
+++ src/stores/userConfig.ts
@@ -0,0 +1,25 @@
+import {defineStore} from "pinia";
+import {computed, reactive, ref} from "vue";
+import type {userConfigStore} from "@/TypeScript/Interfaces/interfaces.ts";
+
+export const userConfigStore = defineStore('userConfig', () => {
+
+    const userConfigData: userConfigStore = reactive({
+        userWidth: window.innerWidth
+    })
+
+    const userWidthGetter: number = computed(() => userConfigData.userWidth)
+
+    const setUserConfigWidth = (userWidthViewIncome: number):void => {
+        userConfigData.userWidth = userWidthViewIncome
+
+        console.log("State Set: ", userConfigData.userWidth)
+    }
+
+
+    return {
+        userConfigData,
+        userWidthGetter,
+        setUserConfigWidth
+    }
+})
