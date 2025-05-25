7fa442543e0b4c5cbbd33d42e27e104fa3e00fc8
Raphael-Maximowski <raphaelmxjv@gmail.com>
2025-05-25 19:56:28 -0300

    feat: Transaction Category

---
+++ src/Data/CategoriesMockUp.ts
@@ -0,0 +1,13 @@
+const categoriesDataMock = {
+    1: { name: 'Housing', color: '#4A90E2' },
+    2: { name: 'Food', color: '#F8A541' },
+    3: { name: 'Transportation', color: '#5E6B73' },
+    4: { name: 'Health', color: '#50C878' },
+    5: { name: 'Entertainment & Leisure', color: '#E91E63' },
+    6: { name: 'Education', color: '#673AB7' },
+    7: { name: 'Shopping & Personal Care', color: '#FF6B6B' },
+    8: { name: 'Debts & Finances', color: '#607D8B' },
+    9: { name: 'Savings & Investments', color: '#4CAF50' }
+}
+
+export default categoriesDataMock
